a246714cb586d28dcaa34ce6ed49f505be6cdc8a
josen <josen@DESKTOP-DR68EKK>
2020-06-04 15:42:31 +0530

    adding initial data

---
+++ Spring-Boot-chuck-norris-jokes-app/src/main/java/com/hardik/chucknorrisjokesapp/SpringBootChuckNorrisJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.chucknorrisjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChuckNorrisJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChuckNorrisJokesAppApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-chuck-norris-jokes-app/src/test/java/com/hardik/chucknorrisjokesapp/SpringBootChuckNorrisJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.chucknorrisjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChuckNorrisJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
